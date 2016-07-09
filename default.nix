d5b43c834ce1f75912370f2b4c777c8f74966804
Michael Alan Dorman <mdorman@ironicdesign.com>
2016-07-09 17:25:51 -0400

    Regenerate nix expression

--- default.nix
+++ default.nix
@@ -1,16 +1,18 @@
 { mkDerivation, aeson, base, conduit, conduit-combinators
-, conduit-extra, hjsonschema, resourcet, stdenv, filepath
+, conduit-extra, filepath, hjsonschema, resourcet, stdenv, text
 , transformers, vector
 }:
 mkDerivation {
   pname = "couch-schema";
   version = "0.0.0.0";
   src = ./.;
-  buildDepends = [
-    aeson base conduit conduit-combinators conduit-extra hjsonschema
-    resourcet filepath transformers vector
+  libraryHaskellDepends = [
+    aeson base conduit conduit-combinators conduit-extra filepath
+    hjsonschema resourcet text transformers vector
   ];
-  testDepends = [ base conduit conduit-combinators resourcet ];
+  testHaskellDepends = [
+    base conduit conduit-combinators resourcet
+  ];
   homepage = "https://github.com/mdorman/couch-schema";
   description = "Schema files for validating CouchDB output";
   license = stdenv.lib.licenses.mit;
